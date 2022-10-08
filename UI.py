9623eabbb62712e4f4455d49f8f7418b72991aea
PranavJore <pranavjore@gmail.com>
2022-10-08 11:58:29 +0530

    created tabs in app

--- UI.py
+++ UI.py
@@ -26,3 +26,10 @@ TAB_CONTROL.add(TAB1, text = 'TAB1')
 TAB_CONTROL.pack(expand=1, fill="both")
 
 v = tk.IntVar()
+
+tk.Label(TAB1,text="""Enter following data - """,justify = tk.LEFT,padx = 20)
+
+b1 = Button(TAB1,text = "CAPTURE FACE",command = func1,activeforeground = "red",activebackground = "pink",pady=10).pack()
+
+b2 = Button(TAB1,text = "FACE DETECT",command = func,activeforeground = "red",activebackground = "pink",pady=10).pack() 
+
