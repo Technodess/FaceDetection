de4251ce3c5c3d38025a4bfa24bbd37cbd1ed13d
Pratibhakachare <89139087+Pratibhakachare@users.noreply.github.com>
2022-11-11 07:35:06 +0000

    while statement changes

--- create_data.py
+++ create_data.py
@@ -29,7 +29,7 @@ if not os.path.isdir(path):
 face_cascade = cv2.CascadeClassifier(haar_file)
 webcam = cv2.VideoCapture(0)
 
-# The program loops until it has 30 images of the face.
+# The programme loops until it has 30 images of the face.
 count = 1
 while count < 50:
     (_, im) = webcam.read()
